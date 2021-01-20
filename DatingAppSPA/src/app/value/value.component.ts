83d63075e7934fc56cc3aaacf8af7f0a82edfa58
ahmed shora <ahmedalshora53@gmail.com>
2021-01-20 16:15:05 +0200

    Initial Commet

---
+++ DatingAppSPA/src/app/value/value.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+  values:any;
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues(){
+    this.http.get('https://localhost:44312/api/values/')
+      .subscribe(res=>this.values=res,err=>console.log(err));
+
+  }
+}
